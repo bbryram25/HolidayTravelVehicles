0ce9771c0e2b47eb49537985f27e90296aada20d
bbryram25 <nichakul.kon@student.mahidol.ac.th>
2025-02-19 18:36:13 +0700

    modified

--- Main.java
+++ Main.java
@@ -1,9 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        // Creating a new Vehicle object
         Vehicle myCar = new Vehicle("NCT127", "Sedan", "Toyota", "CRV", 2023, 3500);
 
-        // Displaying vehicle details
         System.out.println("Vehicle Details:");
         System.out.println("Serial Number: " + myCar.getVehicleSerialNumber());
         System.out.println("Model: " + myCar.getModel());
@@ -12,7 +10,6 @@ public class Main {
         System.out.println("Year: " + myCar.getYear());
         System.out.println("Base Cost: $" + myCar.getBaseCost());
 
-        // Creating a new vehicle record
         myCar.createNewVehicleRecord();
     }
 }
